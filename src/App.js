a01268df9a0851f2793d95db8d0bfff6a6bcaa9b
ImRobotObviously <daniel.contreras.swg@gmail.com>
2019-05-01 17:48:54 -0500

    Inicializando el proyecto

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import 'materialize-css/dist/css/materialize.min.css';
+function App() {
+  return (
+    <div className='row'>
+      <div className="col s1 m12 l12">
+        hola
+      </div>
+      <div className="col s1 m12 l12">
+        hola
+      </div>
+    </div>
+  );
+}
+
+export default App;
